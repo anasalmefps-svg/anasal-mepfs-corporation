6487e180a543e211ccf4bbb41819aedd69f953cd
Anasal Developer <anasalmefps@gmail.com>
2026-03-19 00:04:10 +0800

    api

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT),  // 14813
-    ssl: { rejectUnauthorized: true }   // required by Aiven
+    port: Number(process.env.DB_PORT),
+    ssl: { rejectUnauthorized: true }  
 });
 
 /* ---------------- MULTER CONFIG ---------------- */
